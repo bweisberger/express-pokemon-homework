560d0bf4c7d231726ffe431dd767edc65c6a2ac1
William Weisberger <bill.weisberger@gmail.com>
2019-07-16 14:56:45 -0600

    Commit 4 - show view shows pokemon details

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/pokemon', (req, res)=>{
   res.render('index.ejs', {pokemon: Pokemon})
 });
 
+app.get('/pokemon/:index', (req, res)=>{
+  res.render('show.ejs', {pokemon: Pokemon[req.params.index]})
+});
+
 app.listen(3000,()=>{
   console.log('listening on port 3000')
-})
+});
