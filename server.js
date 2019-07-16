e46663f45ec7fb2a6acbd434718d2c5c268b552a
William Weisberger <bill.weisberger@gmail.com>
2019-07-16 14:43:09 -0600

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
-const Pokemon = require('./models/pokemon.js')
+const Pokemon = require('./models/pokemon')
+
+app.use(express.static('public'));
 
 app.get('/pokemon', (req, res)=>{
-  res.send(Pokemon);
-})
+  res.render('index.ejs', {pokemon: Pokemon})
+});
 
 app.listen(3000,()=>{
   console.log('listening on port 3000')
